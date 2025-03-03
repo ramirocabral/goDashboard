b5c991fd9bcd3e549bb66cf2c0e093d36779514f
ramiro <ramiro.cabral@alu.ing.unlp.edu.ar>
2025-03-03 04:09:50 -0300

    bad request error

--- api/errors.go
+++ api/errors.go
@@ -11,3 +11,9 @@ func (app *app) internalServerError(w http.ResponseWriter, r *http.Request, err 
 
     writeJSONError(w, http.StatusInternalServerError, "Internal server error")
 }
+
+func (app *app) badRequest(w http.ResponseWriter, r *http.Request, message string){
+	logger.GetLogger().Errorf("Bad request, method: %s, path: %s, message: %s", r.Method, r.URL.Path, message)
+
+	writeJSONError(w, http.StatusBadRequest, message)
+}
