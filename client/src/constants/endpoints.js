511b15feb4aef2fe9240160a8c7811dbde1bed0a
ramirocabral <ramiro.cabral@alu.ing.unlp.edu.ar>
2025-03-09 21:11:02 -0300

    fix: fixed api url

--- client/src/constants/endpoints.js
+++ client/src/constants/endpoints.js
@@ -1,4 +1,8 @@
-export const API_BASE_URL = "http://localhost:8080/api/v1";
+// export const API_BASE_URL = "http://localhost:8080/api/v1";
+export const API_BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? window.location.origin + "/api/v1"
+    : "http://localhost:8080/api/v1";
 export const API_BASE_URL_HISTORY = `${API_BASE_URL}/history`;
 export const API_BASE_URL_REALTIME = `${API_BASE_URL}/ws`;
 export const API_BASE_URL_STATS = `${API_BASE_URL}/stat`;
